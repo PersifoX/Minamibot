01ef3a57d8d9b8aee37b8e04bb0e0dede2899b97
Alternative Vladislav <kolodaznyjvladislav@gmail.com>
2025-03-15 18:33:07 +0300

    more debug

--- src/services/rcon.py
+++ src/services/rcon.py
@@ -1,6 +1,9 @@
 from rcon.source import rcon
 
+from _logging import get_logger
 from config import get_settings
+
+logger = get_logger(__name__)
 
 
 class Rcon:
@@ -8,9 +11,19 @@ class Rcon:
         self.config = get_settings()
 
     async def exec(self, command) -> str:
-        return await rcon(
+        logger.info(
+            f"Executing command: {command} on {self.config.minecraft_server_ip}"
+        )
+
+        res = await rcon(
             command=command,
             host=self.config.minecraft_server_ip,
             port=self.config.minecraft_server_port,
             passwd=self.config.minecraft_server_password,
         )
+
+        logger.info(
+            f"Command {command} executed on {self.config.minecraft_server_ip}: {res}"
+        )
+
+        return res
